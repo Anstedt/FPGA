e991de4327793c7fb59796d9a752ba15241acca1
Anstedt <anstedt@gmail.com>
2022-07-21 16:24:18 -0500

    Added comments

--- led_io_control/src/led_io_control.v
+++ led_io_control/src/led_io_control.v
@@ -20,23 +20,26 @@ always @(posedge sys_clk or negedge btn_A) begin
 end
 
 always @(posedge sys_clk or negedge btn_A) begin
-    if (!btn_A)
+    if (!btn_A) // Reset everything if A is pressed, not A is held high by default
         begin
             led <= 6'b111110;
             mygo_right = 0;
         end
     else if (counter == 24'd1349_9999)       // 0.5s delay
         begin
+            // Scheme is to watch for left LED to be on and switch directions, notice 0 is on
             if (led[5] == 0 && mygo_right == 0)
-            begin
+            begin // no pick new direction
                 mygo_right = 1;
                 led <= 6'b011111;
             end
+        // Scheme is to watch for right LED to go on, and switch directions
         else if (led[0] == 0 && mygo_right == 1)
             begin
                 mygo_right = 0;
                 led <= 6'b111110;
             end
+        // Here we just go in the selected direction
         if (mygo_right == 1)
             led[5:0] <= {led[0], led[5], led[4], led[3], led[2], led[1]}; // Right
         else
